b99675c77523b968a27f354e12f94cf4cb9986b6
namikkaya <namikkaya86@gmail.com>
2020-01-06 00:07:14 +0300

    invio v1 - edit

--- KayaChallengeNew/lib/serviceManager.swift
+++ KayaChallengeNew/lib/serviceManager.swift
@@ -181,6 +181,9 @@ class serviceManager: NSObject {
                 
                 //failInfo(id,data)
                 
+                /*
+                 limitlere takıldığında detay için aşağıda ki sample data döndürülür.
+                 */
                 let fakeDataString:String = "{ \"meta\": { \"code\": 200, \"requestId\": \"5997070d6a60717704801060\" }, \"response\": {\"photos\": { \"count\": 1, \"items\": [ { \"id\": \"51e4151c498e60b5d17bc721\",\"createdAt\": 1373902108,\"source\": { \"name\": \"Instagram\",\"url\": \"http://instagram.com\" }, \"prefix\": \"https://igx.4sqi.net/img/general/\", \"suffix\": \"/1022386_wEd7VXnWzp5lajvRLtAPunoiufDSIq8PMEtEuIH2Tzg.jpg\", \"width\": 612, \"height\": 612, \"user\": {\"id\": \"123456\", \"firstName\": \"John- limit dolduğu için fake data\", \"lastName\": \"D\", \"photo\": { \"prefix\": \"https://igx.4sqi.net/img/user/\", \"suffix\": \"/YXNQ5O4TLYSNHMKY.jpg\" } }, \"checkin\": { \"id\": \"51e4151b498ea66547febfaf\", \"createdAt\": 1373902107, \"type\": \"checkin\", \"timeZoneOffset\": -240 }, \"visibility\": \"public\" } ], \"dupesRemoved\": 0 } } }"
                 
                 let dataR = fakeDataString.data(using: .utf8)!
